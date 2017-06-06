f7100b18f9b464d9801227232b31708b1c30dc8c
joaoalcimar <joao.alcimar.junior@gmail.com>
2017-06-06 02:47:59 -0300

    funcao minimo adicionada

--- seach.py
+++ seach.py
@@ -2,6 +2,14 @@
 def searchMax(list):
 	max = int(list[0].strip())
 	for i in range(1,len(lista)):
-		if int list[i] > maior:
-			maior = int(list[i]
-	returun maior
+		if int list[i] > max:
+			max = int(list[i]
+	returun max
+
+#coding UTF-8
+def searchMin(list):
+	min = int(list[0].strip())
+	for i in range(1,len(lista)):
+		if int list[i] < min:
+			min = int(list[i]
+	returun min
